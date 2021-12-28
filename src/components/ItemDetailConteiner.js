9176f3390025f56afa021c902d8179120124398a
Gonzalo Nasello <gonzalonasello67@gmail.com>
2021-12-28 20:19:05 -0300

    Correccion ver mas

--- src/components/ItemDetailConteiner.js
+++ src/components/ItemDetailConteiner.js
@@ -1,15 +1,23 @@
 import { useState, useEffect} from "react/cjs/react.development";
+import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
 
 const detalleProducto =  
-    {nombre: "Crisis en tierras infinitas", precio: 2000, img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/184/069/products/crisis_cov_arg1-a4a9db904b75118e4315980054342033-1024-1024.jpg", id:1}
+[
+    {nombre: "Crisis en tierras infinitas", precio: 2000, img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/184/069/products/crisis_cov_arg1-a4a9db904b75118e4315980054342033-1024-1024.jpg", id:1},
+    {nombre: "The Flash", precio: 1100, img:"https://images-na.ssl-images-amazon.com/images/I/91tVDADj+dL.jpg", id:2},
+    {nombre: "Avengers: El guantelete del infinito", precio: 1800, img:"https://d3ugyf2ht6aenh.cloudfront.net/stores/001/184/069/products/excelsioravengersguanteletedelinfivol011-fba60a45b1ea17e3af15909599812390-1024-1024.jpg", id:3},
+    {nombre: "Civil war", precio:1500, img:"https://dam.smashmexico.com.mx/wp-content/uploads/2019/09/marvel-grandes-eventos-civil-war-portada.jpg", id:4}
+]
     
 
 const ItemDetailContainer = () => {
   
+    const { id } = useParams()
     let [producto, setProducto] = useState([])
 
     useEffect (()=>{
+        
         const promesa = new Promise ((res, rej) =>{
             setTimeout(()=>{
                 res(detalleProducto)
@@ -18,15 +26,18 @@ const ItemDetailContainer = () => {
         })
 
         promesa
-        .then(()=>{
+        .then((res)=>{
             console.log("Bien")
-            setProducto(detalleProducto)
+            if(id){
+                let data = res.find((detalleProducto) => detalleProducto.id === Number(id))
+                setProducto(data)
+            }
         })
         .catch(()=>{
             console.log("Mal")
         })
     },[])
-
+    
     return (
         <div>
             <ItemDetail producto={producto}/>
